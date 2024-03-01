c3e80591e381f0049de1c9a93411a0339fa8aedf
Nursemen <nnursultan07@mail.ru>
2024-03-01 18:34:13 +0600

    last

--- nursiscool/models/ouruser.py
+++ nursiscool/models/ouruser.py
@@ -2,12 +2,18 @@ from django.db import models
 
 # Create your models here.
 from django.conf import settings
-from django.utils import timezone
 
 class OurUser(models.Model):
     name = models.CharField(max_length=250)
-    password = models.IntegerField(max_length =10)
+    password = models.CharField(max_length=25)
     rating = models.FloatField()
+    role_choices = {
+        "1": "guest",
+        "2": "admin",
+        "3": "worker",
+    }
+    role = models.CharField(max_length=1, choices=role_choices, default=role_choices["1"])
+    profile_img = models.ImageField(upload_to="users/profiles/", blank=True, null=True)
 
-def str(self):
-    return self.name
+    def __str__(self):
+        return self.name
