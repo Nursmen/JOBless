8b8bb36c29f27faa3a71ee9c4618455fa4dc035b
Nursemen <nnursultan07@mail.ru>
2024-03-01 17:18:16 +0600

    secondd

--- nursiscool/models.py
+++ nursiscool/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class OurUser(models.Model):
+    name = models.CharField(max_length=250)
+    password = models.IntegerField(max_length =10)
+    rating = models.FloatField()
+
+def str(self):
+    return self.name
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length =250)
+    description = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
